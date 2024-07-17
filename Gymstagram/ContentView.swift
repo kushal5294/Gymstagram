8d13a4501337c576d0864159f99b86d20a7b8408
Kushal Patel <patelku@umich.edu>
2024-07-16 21:10:32 -0400

    Initial Commit

---
+++ Gymstagram/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gymstagram
+//
+//  Created by Kushal Patel on 7/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gymstagram/GymstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymstagramApp.swift
+//  Gymstagram
+//
+//  Created by Kushal Patel on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct GymstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
